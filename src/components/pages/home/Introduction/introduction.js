1868ffd2c10cfe77f90fe2f76dc12f0ab5a99b27
Nguyen Phan <nguyenpt@rikkeisoft.com>
2020-09-25 14:34:15 +0700

    call api, fill data

--- src/components/pages/home/Introduction/introduction.js
+++ src/components/pages/home/Introduction/introduction.js
@@ -1,24 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './introduction.scss'
+import APP_CONSTANT from '../../../../app/consts'
 
-function Introduction() {
+function Introduction(props) {
+    const {introduction} = props;
+
+    const linkSocial = introduction && introduction.socials && Object.keys(introduction.socials).map((key, index) => {
+        return <a key={index} href={introduction.socials[key]}><i className={`ion-social-${key}-outline`}/></a>
+    })
 
     return (
         <div className="introduction_area text-center background-overlay"
-             style={{backgroundImage: 'url(img/avt.jpg)'}}>
+             style={{backgroundImage: `url(${APP_CONSTANT.Avatar})`}}>
             <div className="classy-table">
                 <div className="classy-table-cell">
                     <div className="introduction_text_area">
                         <div className="logo"/>
-                        <h2>I'm Nguyen Phan Thanh</h2>
-                        <p>And I'm a Senior Website Developer</p>
-                        {/* Social Icon Area Start */}
+                        <h2>I'm {introduction.full_name}</h2>
+                        <p>{introduction.short_text}</p>
+
                         <div className="resume_social_icon">
-                            <a href="#"><i className="ion-social-facebook-outline"/></a>
-                            <a href="#"><i className="ion-social-twitter-outline"/></a>
-                            <a href="#"><i className="ion-social-googleplus-outline"/></a>
-                            <a href="#"><i className="ion-social-instagram-outline"/></a>
-                            <a href="#"><i className="ion-social-youtube-outline"/></a>
+                            {linkSocial}
                         </div>
                     </div>
                 </div>
